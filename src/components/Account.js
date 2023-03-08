967b4a2fa326f6a31061319f8b0bd5a2ce32e2eb
Van Binh <nguyenvanbinh13112000@outlook.com>
2023-03-08 12:34:10 +0700

    style: Account

--- src/components/Account.js
+++ src/components/Account.js
@@ -5,15 +5,19 @@ import User from './User';
 
 const Account = () => {
   return (
-    <Wrapper>
-      <User />
-      <Followers />
-    </Wrapper>
+    <section className='section'>
+      <Wrapper className='section-center'>
+        <User />
+        <Followers />
+      </Wrapper>
+    </section>
   );
 };
 
 const Wrapper = styled.div`
+  padding-top: 2rem;
   display: grid;
+  gap: 3rem 2rem;
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
